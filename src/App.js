8cb8ff7fbfbc39f9bb326b4ada50d9ebf6847d8f
bin2209 <binazure@icloud.com>
2024-05-11 15:41:57 +0700

    icon weather

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
 import Search from './components/search';
+import CurrentWeather from './components/current-weather/current-weather';
+
 
 function App() {
   const handleOnSearchChange = (searchData) =>{
-    console.log(searchData);
+    console.log(searchData);  
   }
   return (
     <div className="container">
+
       <Search onSearchChange={handleOnSearchChange}/>
+      <CurrentWeather/>
+   
     </div>
   );
 }
---
+++ src/components/current-weather/current-weather.js
@@ -0,0 +1,16 @@
+import "../current-weather/current-weather.css"
+
+
+const CurrentWeather = () =>{
+    return (
+        <div className="weather">
+            <div className="top">
+                <p className="city">Da Nang</p>
+                <p className="weather-description">Sunny</p>
+            </div>
+            <img alt="weather" className="weather-icon" src="icons/01d.png"/>
+        </div>
+    );
+} 
+
+export default CurrentWeather;
