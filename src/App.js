1507aa90bc6545ece7629d40cd83e856b8e52f7d
bin2209 <binazure@icloud.com>
2024-05-12 09:42:14 +0700

    install Accordion node js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Search from './components/search';
 import CurrentWeather from './components/current-weather/current-weather';
 import { WEATHER_API_URL, WEATHER_API_KEY } from './api';
 import { useState } from 'react';
+import Forecast from './components/forecast/forecast';
 
 
 function App() {
@@ -36,7 +37,7 @@ function App() {
 
       <Search onSearchChange={handleOnSearchChange} />
       {currentWeather && <CurrentWeather data={currentWeather} />}
-
+      {forecast && <Forecast data={forecast}/>}
     </div>
   );
 }
---
+++ src/components/forecast/forecast.js
@@ -0,0 +1,15 @@
+import { Accordion } from "accordion";
+
+const Forecast = ({data}) => {
+    return (
+        <>
+            <label className="title">Daily</label>
+            <Accordion allowZeroExpanded>
+
+            </Accordion>
+        
+        </>
+    );
+}
+
+export default Forecast;
