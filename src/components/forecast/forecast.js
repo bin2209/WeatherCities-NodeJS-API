b700cc4a44e657a24b76485495bfb475244ec23f
bin2209 <binazure@icloud.com>
2024-05-12 09:48:15 +0700

    npm install --save react-accessible-accordion

--- src/components/forecast/forecast.js
+++ src/components/forecast/forecast.js
@@ -5,6 +5,12 @@ const Forecast = ({data}) => {
         <>
             <label className="title">Daily</label>
             <Accordion allowZeroExpanded>
+            {data.list.splice(0,7).map((item, idx) =>{
+                <AccordionItemHeading>
+                    
+                </AccordionItemHeading>
+            })}
+            <AccordionItem></AccordionItem>
 
             </Accordion>
         
